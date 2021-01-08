44087abd9b5913472967909a5fa24cb31efce443
Jashan Chahal <chahaljashan123@gmail.com>
2021-01-08 20:01:09 +0530

    Ui changes and added social media icons

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,23 @@
-import { AnimatedText } from "./AnimatedText"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { AnimatedText } from "./AnimatedText";
+import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 export const Intro = () => {
     return (
-        <div className="Intro">
-           <AnimatedText textColor = "#f09f73" overlayColor="#f58c53">
-            <h1>Hello!</h1>
-            <h1>I'm Jashan Chahal, a Software Engineer</h1>
-            <h1>I love to explore and build exciting things!</h1>
-            </AnimatedText>
+        <div>
+            <FontAwesomeIcon className="m-2" color="#b34b29" size="2x" icon={faChevronCircleDown} />
+            <FontAwesomeIcon className="m-2" color="#b34b29" size="2x" icon={faGithub} />
+            <FontAwesomeIcon className="m-2" color="#b34b29" size="2x" icon={faLinkedin} />
+            <FontAwesomeIcon className="m-2" color="#b34b29" size="2x" icon={faInstagram} />
+            <FontAwesomeIcon className="m-2" color="#b34b29" size="2x" icon={faFacebook} />
+
+            <div className="Intro">
+                <AnimatedText textColor="#b34b29" overlayColor="#ebcac0">
+                    <h1>Hello!</h1>
+                    <h1>I'm Jashan Chahal, a Software Engineer</h1>
+                    <h1>I love to explore and build exciting things!</h1>
+                </AnimatedText>
+            </div>
         </div>
     )
 }
