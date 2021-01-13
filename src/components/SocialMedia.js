042595ac5add740d4d4e1c7e9c48baa78684371c
Jashan Chahal <chahaljashan123@gmail.com>
2021-01-13 18:25:14 +0530

    removed css transition group dependency

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronCircleRight, faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { useState } from "react";
 import './socialMedia.css';
 export const SocialMedia = (props) => {
@@ -20,18 +19,14 @@ export const SocialMedia = (props) => {
                 icon={chevronCirle}
                 onClick={() => (setMore(!more))}
             />
-            <TransitionGroup component={null}>
-                {more && icons.map((icon, idx) => (
-                    <CSSTransition key={idx} in={more} timeout={350} classNames="icon">
-                        <FontAwesomeIcon
-                            className={props.style.bootstrapMargin}
-                            color={props.style.textColor}
-                            size="2x"
-                            icon={icon} />
-                    </CSSTransition>
-                ))
-                }
-            </TransitionGroup>
+            {more && icons.map((icon, idx) => (
+                <FontAwesomeIcon
+                    className={props.style.bootstrapMargin}
+                    color={props.style.textColor}
+                    size="2x"
+                    icon={icon} />
+            ))
+            }
 
         </>
     )
